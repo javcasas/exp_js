d276b16d9927a9f16e897b48f6b9466a38fd3d06
dariojcalo91 <dark_raven_d@hotmail.com>
2018-03-12 11:01:13 -0500

    Extracting the validator to it's own function.

--- public/javascripts/validador/validadorPlaca.js
+++ public/javascripts/validador/validadorPlaca.js
@@ -8,9 +8,15 @@ const router = express.Router();
 
 const testbdd = require('../control/bddconn');
 
+let valdiarLength = function(value){
+    console.log('valor validar: '+value);
+    return value.length !== 8;
+};
+
 router.get("/", function(req, res, next){
     const ppIn = req.query.placaIn;
     console.log(req.query.placaIn);
+
     let promesa1 = new Promise((resolve, reject)=>{
         setTimeout(function(){
             resolve("Success!");
@@ -20,14 +26,14 @@ router.get("/", function(req, res, next){
     console.log("Redir despues de 1 sec..");
 
     promesa1.then((successMessage)=> {
-        if(ppIn.length !== 8){
+        if (valdiarLength(ppIn)){
             const err = new Error('Error de longitud');
             err.status = 400;
             next(err);
-        } else{
-            res.locals.placaver = 'A: '+ ppIn;
+        } else {
+            res.locals.placaver = 'A: '+ppIn;
             res.render('ppform');
-        };
+        }
         console.log("Yay! "+successMessage);
     });
 });
