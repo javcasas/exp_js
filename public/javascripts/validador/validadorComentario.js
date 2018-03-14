f4004242f411a2357c19ad3bf2beb83f38dd4d90
dariojcalo91 <dark_raven_d@hotmail.com>
2018-03-14 14:35:08 -0500

    Correction: on validateNonEmpty is necesary 2 return (one for returning promise, other for return reject o fulfill on promise block) correction on promise for validate email and comment.

--- public/javascripts/validador/validadorComentario.js
+++ public/javascripts/validador/validadorComentario.js
@@ -13,10 +13,15 @@ function isEmpty(x) {
 }
 
 function validateNonEmpty(variable, errorMessage, res) {
-    if (isEmpty(variable)) {
-        res.locals.emailarm = errorMessage;
-        res.render('comform');
-    }
+    return new Promise((fullfill, reject) => {
+        if (isEmpty(variable)) {
+            res.locals.emailarm = errorMessage;
+            //return reject(new Error('Fail:')) buena practica
+            return reject('Fail');
+        } else {
+            return fullfill()
+        }
+    })
 }
 
 function createComment (req, res, next) {
@@ -25,11 +30,22 @@ function createComment (req, res, next) {
 
     res.locals.comments = [];
 
-    validateNonEmpty(email, "Email no provisto", res);
-    validateNonEmpty(comment, "Comentario no provisto", res);
-
+    new Promise((fullfill, reject) => fullfill())
+        .then(() => console.log("Started"))
+        .then(() =>validateNonEmpty(email, "Email no provisto", res)) // Validar email
+        .then(() =>validateNonEmpty(comment, "Comentario no provisto", res))  // Validar comentario
+        .then(() => Info.create({
+            correo: email,
+            comentario: comment
+        }))  // Insertar en la BBDD
+        .then(() => Info.all())  // Traer todos los comentarios de la BBDD
+        .then(comments => {
+            res.locals.comments = comments
+            res.render('comform')
+        })
+        .catch((err) => console.error("something went wrong: ", err)) // Algo fallo
     // Email and comment are fine. Proceed to write them to the DB
-    const newComment = Info.create({
+    /*const newComment = Info.create({
         correo: email,
         comentario: comment
     })
@@ -37,7 +53,7 @@ function createComment (req, res, next) {
     .then(comments => {
         res.locals.comments = comments
         res.render('comform')
-    })
+    })*/
 }
 
 //route
